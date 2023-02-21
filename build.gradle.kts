65ac68eede24361ff5b101e34e424f3dc3ce968a
Christian Schierle <csc@scireum.de>
2023-02-21 12:51:09 +0100

    Bumps plugin versions
    Fixes: SIRI-723

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.7.10"
-    id("com.gradle.plugin-publish") version "1.0.0"
+    kotlin("jvm") version "1.8.10"
+    id("com.gradle.plugin-publish") version "1.1.0"
 }
 
 repositories {
